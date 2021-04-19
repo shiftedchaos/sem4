0d24f31c17f866b2fd92540df29abfc79a4c4c46
shiftedchaos <ars191101@gmail.com>
2021-04-20 00:29:08 +0300

    commit

--- 4th task/task1.cpp
+++ 4th task/task1.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+
+bool eratosthenesSieve(int currentNumber)
+{
+    /*
+    Describe the function that checks if the number is prime and returns a boolean value.
+    This function is based on the "sieve of Eratosthenes" algorithm.
+    True - if the number is prime, false - normal.
+    */
+
+    // If the number equals to 1 or if it even, return false. And if it equals to 2, return true.
+    if (currentNumber == 2 or currentNumber == 3) {
+        return true;
+    } else if (!(currentNumber % 2) or !(currentNumber % 3) or currentNumber == 1) {
+        return false;
+    }
+
+    // If the number has the divider from 5 to the root of this number with the second step return false.
+    for (int divider = 5; divider <= sqrt(currentNumber); divider += 2) {
+        if (!(currentNumber % divider)) {
+            return false;
+        }
+    }
+
+    // In all other situations, return true.
+    return true;
+}
+
+bool isOdd (int elem)
+{
+  return elem % 2 == 1;
+}
+
+//Task 1
 
 int main()
 {
@@ -31,4 +65,22 @@ int main()
   {
     std::cout << elem << ' ';
   }
+
+  //Task 2
+
+  int num;
+  num = count_if(coll.begin(), coll.end(), isOdd);
+  std::cout << "Amount of odd elements in coll: " << num << std::endl;
+
+  std::cout << "Minimum: " << *min_element(coll.begin(), coll.end()) <<
+  std::endl;
+  std::cout << "Maximum: " << *max_element(coll.begin(), coll.end()) <<
+  std::endl;
+
+  std::vector<int>::iterator pos;
+  pos = find_if(coll.begin(), coll.end(), eratosthenesSieve);
+  std::cout << "Just one prime number from coll: " << std::distance(coll.begin(), pos) + 1 << std::endl;
+
+
+
 }
